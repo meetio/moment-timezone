eb4bb7bd2c3db03f6f82877f71e9f852fb0e44f5
Johan Nordstrom <johan@digitalvillage.se>
2016-10-24 20:19:40 +0200

    Added tests for 2016h database release.

---
+++ tests/zones/asia/yangon.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var helpers = require("../../helpers/helpers");
+
+exports["Asia/Yangon"] = {
+	"1919" : helpers.makeTestYear("Asia/Yangon", [
+		["1919-12-31T17:35:19Z", "23:59:59", "RMT", -23080 / 60],
+		["1919-12-31T17:35:20Z", "00:05:20", "BURT", -390]
+	]),
+
+	"1942" : helpers.makeTestYear("Asia/Yangon", [
+		["1942-04-30T17:29:59Z", "23:59:59", "BURT", -390],
+		["1942-04-30T17:30:00Z", "02:30:00", "JST", -540]
+	]),
+
+	"1945" : helpers.makeTestYear("Asia/Yangon", [
+		["1945-05-02T14:59:59Z", "23:59:59", "JST", -540],
+		["1945-05-02T15:00:00Z", "21:30:00", "MMT", -390]
+	])
+};
--- tests/zones/pacific/fakaofo.js
+++ tests/zones/pacific/fakaofo.js
@@ -4,7 +4,7 @@ var helpers = require("../../helpers/helpers");
 
 exports["Pacific/Fakaofo"] = {
 	"2011" : helpers.makeTestYear("Pacific/Fakaofo", [
-		["2011-12-30T10:59:59+00:00", "23:59:59", "TKT", 660],
-		["2011-12-30T11:00:00+00:00", "00:00:00", "TKT", -780]
+		["2011-12-30T10:59:59Z", "23:59:59", "TKT", 660],
+		["2011-12-30T11:00:00Z", "00:00:00", "TKT", -780]
 	])
 };
